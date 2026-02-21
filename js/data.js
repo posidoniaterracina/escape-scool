3e16118c9c514d95b225955687fec1bb9f25fb56
posidoniaterracina <umberto.baroni@unipam.it>
2026-02-21 15:23:13 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,75 @@
+const domande = {
 
+    informatica: [
+        {
+            testo: "Quale linguaggio struttura una pagina web?",
+            opzioni: ["CSS", "HTML", "Python", "Java"],
+            corretta: 1
+        },
+        {
+            testo: "Quale comando SQL seleziona dati?",
+            opzioni: ["GET", "SELECT", "SHOW", "PICK"],
+            corretta: 1
+        }
+    ],
+
+    economia: [
+        {
+            testo: "Il bilancio comprende:",
+            opzioni: [
+                "Stato Patrimoniale e Conto Economico",
+                "Solo fatture",
+                "Registro IVA",
+                "Libro paga"
+            ],
+            corretta: 0
+        },
+        {
+            testo: "L'IVA è:",
+            opzioni: [
+                "Imposta diretta",
+                "Imposta indiretta",
+                "Contributo volontario",
+                "Tassa scolastica"
+            ],
+            corretta: 1
+        }
+    ],
+
+    diritto: [
+        {
+            testo: "Il GDPR tutela:",
+            opzioni: [
+                "Scuola privata",
+                "Dati personali",
+                "Reati penali",
+                "Contratti pubblici"
+            ],
+            corretta: 1
+        },
+        {
+            testo: "Un contratto è valido con:",
+            opzioni: [
+                "Accordo tra le parti",
+                "Firma del sindaco",
+                "Notaio obbligatorio",
+                "Registro tribunale"
+            ],
+            corretta: 0
+        }
+    ],
+
+    inglese: [
+        {
+            testo: "Invoice significa:",
+            opzioni: ["Fattura", "Pagamento", "Scadenza", "Contratto"],
+            corretta: 0
+        },
+        {
+            testo: "Deadline significa:",
+            opzioni: ["Scadenza", "Firma", "Pagamento", "Contratto"],
+            corretta: 0
+        }
+    ]
+
+};
